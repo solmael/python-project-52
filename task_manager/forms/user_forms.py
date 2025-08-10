5963dca9e2d1a73c0bf7bb53f71e3a75de1226fa
solmael <solmael13@gmail.com>
2025-08-10 15:31:09 +0300

    user_forms edited

--- task_manager/forms/user_forms.py
+++ task_manager/forms/user_forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from task_manager.models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     full_name = forms.CharField()
