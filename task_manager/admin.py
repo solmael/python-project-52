c1b0db859774b00f881ac602d894c1319959faec
solmael <solmael13@gmail.com>
2025-08-15 14:44:56 +0300

    task_manager/admin.py edited

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-from .models import CustomUser, Task, Status, Label
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, Task, Status, Label
 
-admin.site.register(CustomUser)
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_full_name')
+    list_select_related = ('profile',)
+    
+    def get_full_name(self, user):
+        return user.profile.full_name
+    get_full_name.short_description = 'Full name'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+# Отмените регистрацию стандартного User и зарегистрируйте с профилем
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Task)
 admin.site.register(Status)
 admin.site.register(Label)
