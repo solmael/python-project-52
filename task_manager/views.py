616e9323e95ddc9776897c1b635b69c2fb01a2de
solmael <solmael13@gmail.com>
2025-08-18 12:59:10 +0300

    views edited

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,34 +1,42 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms.user_forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
+from .forms.user_forms import CustomUserChangeForm, CustomUserCreationForm
 
 User = get_user_model()
+
+
 def index(request):
     return render(request, 'task_manager/index.html')
+
 
 def users(request):
     return render(request, 'users.html')
 
+
 def login_view(request):
     return render(request, 'login.html')
 
+
 def register_view(request):
     return render(request, 'register.html')
+
 
 class UsersListView(ListView):
     model = User
     template_name = 'task_manager/users.html'
     context_object_name = 'users'
 
+
 class UserCreateView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'task_manager/register.html'
     success_url = reverse_lazy('login')
+
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
@@ -38,6 +46,7 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     
     def get_queryset(self):
         return super().get_queryset().filter(id=self.request.user.id)
+
 
 class UserDeleteView(LoginRequiredMixin, DeleteView):
     model = User
