f655b8d21ca8cdc75732eb882ce08f062157fd1b
solmael <solmael13@gmail.com>
2025-08-18 12:57:11 +0300

    migrations edited

--- task_manager/migrations/0001_initial.py
+++ task_manager/migrations/0001_initial.py
@@ -10,7 +10,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
