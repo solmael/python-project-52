e5cbd0f312f1db90f11300520ace3ebe64f4a633
solmael <solmael13@gmail.com>
2025-08-20 20:16:11 +0300

    settings edited

--- task_manager/models/user.py
+++ task_manager/models/user.py
@@ -8,20 +8,3 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.full_name
-
-
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    status = models.ForeignKey('Status', on_delete=models.CASCADE)
-    labels = models.ManyToManyField('Label')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Status(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class Label(models.Model):
-    name = models.CharField(max_length=50)
