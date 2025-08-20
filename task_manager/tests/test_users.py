43e7fbb940cb12c984280302113ec9085fbcefaf
solmael <solmael13@gmail.com>
2025-08-20 16:20:40 +0300

    some teests

--- task_manager/tests/test_users.py
+++ task_manager/tests/test_users.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
@@ -10,13 +11,12 @@ User = get_user_model()
 class UserCRUDTest(TestCase):
     def setUp(self):
         self.client = Client()
-
+        self.username = f'testuser_{uuid.uuid4().hex[:8]}'
+        self.password = 'Newpass123+!'
         self.user = User.objects.create_user(
-            username='testuser',
-            password='testpass'
+            username=self.username,
+            password=self.password
         )
-
-        self.profile = Profile.objects.create(user=self.user, full_name='Test User')
 
     def test_user_list_view(self):
         response = self.client.get(reverse('users'))
@@ -27,10 +27,43 @@ class UserCRUDTest(TestCase):
             'username': 'newuser',
             'first_name': 'New',
             'last_name': 'User',
-            'password1': 'Newpass123+',
-            'password2': 'Newpass123+'
+            'password1': 'Newpass123+!',
+            'password2': 'Newpass123+!'
         }
         response = self.client.post(reverse('user_create'), data)
         self.assertRedirects(response, reverse('login'))
 
-    
+    def test_user_update_view(self):
+        self.client.login(username=self.username, password=self.password)
+        
+        data = {
+            'username': self.username,
+            'first_name': 'Updated',
+            'last_name': 'User'
+        }
+        response = self.client.post(reverse('user_update', args=[self.user.id]), data)
+        self.assertRedirects(response, reverse('users'))
+        
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'Updated')
+
+    def test_login_success(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': self.password
+        })
+        
+        self.assertRedirects(response, reverse('home'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_logout_success(self):
+        self.client.login(username=self.username, password=self.password)
+        
+        response = self.client.get(reverse('home'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+        
+        response = self.client.post(reverse('logout'), follow=True)
+        self.assertRedirects(response, reverse('home'))
+        
+        response = self.client.get(reverse('home'))
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
