2507bc0be0cfee11899a859b269c8373bd1c0a95
solmael <solmael13@gmail.com>
2025-08-25 17:17:46 +0300

    added status models

--- task_manager/models/__init__.py
+++ task_manager/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import Profile
+from .status import Status
 
-__all__ = ['Profile']
+__all__ = ['Profile', 'Status']
--- task_manager/models/status.py
+++ task_manager/models/status.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.db.models.deletion import ProtectedError
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    # def delete(self, using=None, keep_parents=False):
+    #     if self.task_set.exists():
+    #         raise ProtectedError(
+    #             "Статус в задачах",
+    #             self.task_set.all()
+    #         )
+    #     super().delete(using=using, keep_parents=keep_parents)
