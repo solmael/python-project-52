da5c9f1c4bc518d06cabf5ebea930806ebcdf566
solmael <solmael13@gmail.com>
2025-08-27 12:00:09 +0300

    urls edited

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -28,7 +28,12 @@ from .views import (
     StatusCreateView,
     StatusUpdateView,
     StatusDeleteView,
-    CustomLoginView
+    CustomLoginView,
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskDetailView
 )
 
 urlpatterns = [
@@ -46,6 +51,13 @@ urlpatterns = [
     path('statuses/create/', StatusCreateView.as_view(), name='status_create'),
     path('statuses/<int:pk>/update/', StatusUpdateView.as_view(), name='status_update'),
     path('statuses/<int:pk>/delete/', StatusDeleteView.as_view(), name='status_delete'),
+    
+    # tasks
+    path('tasks/', TaskListView.as_view(), name='tasks'),
+    path('tasks/create/', TaskCreateView.as_view(), name='task_create'),
+    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
     
     # auth
     path('login/', CustomLoginView.as_view(
