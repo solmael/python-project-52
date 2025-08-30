b12df06fabf6f784ceb36d66705932f3aa7537e4
solmael <solmael13@gmail.com>
2025-08-30 11:19:49 +0300

    edited nigrations by ruff

--- task_manager/migrations/0001_initial.py
+++ task_manager/migrations/0001_initial.py
@@ -17,17 +17,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Status',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, unique=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('id', models.BigAutoField(
+                    auto_created=True, 
+                    primary_key=True, 
+                    serialize=False, 
+                    verbose_name='ID'
+                    )),
+                ('name', models.CharField(
+                    max_length=255, unique=True
+                    )),
+                ('created_at', models.DateTimeField(
+                    auto_now_add=True
+                    )),
             ],
         ),
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('full_name', models.CharField(max_length=255)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
+                ('id', models.BigAutoField(
+                    auto_created=True, 
+                    primary_key=True, 
+                    serialize=False, 
+                    verbose_name='ID'
+                    )),
+                ('full_name', models.CharField(
+                    max_length=255
+                    )),
+                ('user', models.OneToOneField(
+                    on_delete=django.db.models.deletion.CASCADE, 
+                    related_name='profile', 
+                    to=settings.AUTH_USER_MODEL
+                    )),
             ],
         ),
     ]
--- task_manager/migrations/0002_task.py
+++ task_manager/migrations/0002_task.py
@@ -16,14 +16,31 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, 
+                    primary_key=True, 
+                    serialize=False, 
+                    verbose_name='ID'
+                    )),
                 ('name', models.CharField(max_length=255)),
                 ('description', models.TextField(blank=True, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='author_tasks', to=settings.AUTH_USER_MODEL)),
-                ('executor', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='executor_tasks', to=settings.AUTH_USER_MODEL)),
-                ('status', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='task_manager.status')),
+                ('author', models.ForeignKey(
+                    on_delete=django.db.models.deletion.PROTECT, 
+                    related_name='author_tasks', 
+                    to=settings.AUTH_USER_MODEL
+                    )),
+                ('executor', models.ForeignKey(
+                    blank=True, null=True, 
+                    on_delete=django.db.models.deletion.SET_NULL, 
+                    related_name='executor_tasks', 
+                    to=settings.AUTH_USER_MODEL
+                    )),
+                ('status', models.ForeignKey(
+                    on_delete=django.db.models.deletion.PROTECT, 
+                    to='task_manager.status'
+                    )),
             ],
         ),
     ]
