8f511346a5a54568a2c180307c5a8c5ad6466e33
solmael <solmael13@gmail.com>
2025-08-30 11:29:22 +0300

    models edited by ruff

--- task_manager/models/user.py
+++ task_manager/models/user.py
@@ -3,7 +3,11 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='profile'
+        )
     full_name = models.CharField(max_length=255)
     
     def __str__(self):
